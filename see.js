e876971117469a2558b58b77b6e420496dc23d3a
mhPavel786 <mhpavel786@gmaiil.com>
2021-05-06 01:24:25 +0600

    first commit

---
+++ see.js
@@ -0,0 +1,15 @@
+        
+        var jobPaiso = false;
+        var savings = 20000;
+        if ( jobPaiso == true && savings > 500000) {
+            console.log(" Joldi patri khuja shuru korbo ");
+        }
+        else if( jobPaiso == true) {
+            console.log(" Pocket e tk paisha jomao beshi kore ");
+        }
+        else if(savings > 500000) {
+            console.log(" Job Khujo ekta valo trpr vaba jabe ");
+        }
+        else{
+            console.log(" Tor kopale biya nai ");
+        }
